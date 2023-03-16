01f69e50d268711e8aa92069bc14e9cdbc13cd35
Adrian <alinasrat95@gmail.com>
2023-03-16 19:12:44 +0100

    Minor change in DecipherThis .cs

--- DeciptherThis.cs
+++ DeciptherThis.cs
@@ -1,47 +1,46 @@
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
+using NUnit.Framework;
 
 public class Kata
 {
   public static string DecipherThis(string s)
   {
-      StringBuilder sb = new StringBuilder();
-			string[] words = s.Split(' ');
-			if (s.Length > 0)
-			{
-				foreach (string word in words)
-				{
-					string resultString = Regex.Match(word, @"\d+").Value;
-					int i = Convert.ToInt32(resultString);
-					string converted = Char.ConvertFromUtf32(i);
+    StringBuilder sb = new StringBuilder();
+    string[] words = s.Split(' ');
+    if (s.Length > 0)
+    {
+      foreach (string word in words)
+      {
+        string resultString = Regex.Match(word, @"\d+").Value;
+        int i = Convert.ToInt32(resultString);
+        string converted = Char.ConvertFromUtf32(i);
 
-					string newString = word.Substring(resultString.Length);
-					string result = converted + newString;
+        string newString = word.Substring(resultString.Length);
+        string result = converted + newString;
 
-					if (result.Length > 1)
-					{
-						char[] charArray = result.ToCharArray();
-						char temp = charArray[1];
-						charArray[1] = charArray[result.Length - 1];
-						charArray[result.Length - 1] = temp;
-						result = new string(charArray);
-					}
+        if (result.Length > 1)
+        {
+          char[] charArray = result.ToCharArray();
+          char temp = charArray[1];
+          charArray[1] = charArray[result.Length - 1];
+          charArray[result.Length - 1] = temp;
+          result = new string(charArray);
+        }
 
-					sb.Append(result);
-					sb.Append(' ');
-				}
-			}
-			else
-			{
-				return "";
-			}
+        sb.Append(result);
+        sb.Append(' ');
+      }
+    }
+    else
+    {
+      return "";
+    }
 
-			return sb.ToString().Trim();
+    return sb.ToString().Trim();
   }
 }
-
-using NUnit.Framework;
 
 [TestFixture]
 public class DecipherThisTests
