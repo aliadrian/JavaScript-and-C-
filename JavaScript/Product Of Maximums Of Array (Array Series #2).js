56a68aa4d8cb9f11a4f2b3c9a1463f18784a86c0
Adrian <adriannasrat@gmail.com>
2024-02-05 00:32:40 +0100

    Product Of Maximums Of Array

---
+++ JavaScript/Product Of Maximums Of Array (Array Series #2).js
@@ -0,0 +1,29 @@
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is  8 * 9 * 10 = 720 .
+// maxProduct ({10, 8, 3, 2, 1, 4, 10}, 5) ==> return (9600)
+// Explanation:
+// Since the size (k) equal 5 , then the subsequence of size 5 whose gives product of maxima is  10 * 10 * 8 * 4 * 3 = 9600 .
+// maxProduct ({-4, -27, -15, -6, -1}, 2) ==> return (4)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is  -4 * -1 = 4 .
+// maxProduct ({10, 3, -1, -27} , 3)  return (-30)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is 10 * 3 * -1 = -30 .
+
+function maxProduct(numbers, size) {
+  if (size <= 0 || size > numbers.length) {
+    throw new Error("Invalid size");
+  }
+
+  const orderedList = numbers.slice().sort((a, b) => b - a);
+  const selectedNumbers = orderedList.slice(0, size);
+
+  const result = selectedNumbers.reduce((a, b) => a * b, 1);
+
+  return result;
+}
